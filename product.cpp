73a428525c3bdd1a91d9b53233976ee7fa0ddbb5
NirmalNivithaa <92011024+NirmalNivithaa@users.noreply.github.com>
2021-10-06 12:31:18 +0530

    product.cpp
    this file execute the product of two numbers

---
+++ product.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float num1,num2,product;
+    cout<<"\n Enter a number1:";
+    cin>>num1;
+    cout<<"\n Enter a number2:";
+    cin>>num2;
+    product=num1*num2;
+    cout<<"\n The product of"<<num1<<"and"<<num2"is"<<product;
+}    
